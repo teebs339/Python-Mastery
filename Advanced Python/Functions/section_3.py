241b2f4e1aeb93c17cab424d28616d010a31dcdd
teebs339 <teeb.shahid@gmail.com>
2025-10-27 18:57:36 +0400

    Advanced python started

---
+++ Advanced Python/Functions/section_3.py
@@ -0,0 +1,33 @@
+# ------------------------------------------------------------------
+# TOPIC 6 - X NO. OF ARGUMENTS
+
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+
+
+# print(f"The multiplied value is: {multiply(2, 3, 4, 5)}")
+
+# ------------------------------------------------------------------
+# TOPIC 7 - XX NO. OF ARGUMENTS -> Otherwise known as dictionary
+
+# def save_user(**user):
+#     # print(user)
+#     print(user["name"])
+
+
+# save_user(name="Ateeb", id=1, age=69)
+
+# ------------------------------------------------------------------
+# TOPIC 8 - SCOPE -> Otherwise known as local variables
+
+# message = "a"
+
+
+# def greet():
+#     message = "b"
+
+# greet()
+# print(message)
