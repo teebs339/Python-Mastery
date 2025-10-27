5550d0343dcf2dacc875a03e99a8dd828a493ef1
teebs339 <teeb.shahid@gmail.com>
2025-10-27 17:48:53 +0400

    Section 3 of the course complete

---
+++ section_3.py
@@ -0,0 +1,56 @@
+# TOPIC 1 - FUNCTIONS
+
+# def greet():
+#     print("yo wassup\none sexy mofo here")
+
+
+# greet()
+
+# ------------------------------------------------------------------
+# TOPIC 2 - ARGUMENTS
+
+# def greet(first_name, last_name):
+#     print(f"{first_name} {last_name}")
+
+
+# greet("toob", "shahid")
+
+# ------------------------------------------------------------------
+# TOPIC 3 - TYPES OF FUNCTION -> function that returns a value
+
+# def get_greeting(name):
+#     return f"yo its yo boy {name}"
+
+
+# message = get_greeting("ateeb")
+# print(message)
+
+# ------------------------------------------------------------------
+# TOPIC 4 - KEYWORD ARGUMENTS
+
+# def increment(number1, number2):
+#     return number1 + number2
+
+
+# print(increment(number1=2, number2=1))
+
+# ------------------------------------------------------------------
+# TOPIC 5 - DEFAULT ARGUMENTS
+
+# def increment(number1, number2=69):
+#     return number1 + number2
+
+
+# print(increment(1))
+
+# ------------------------------------------------------------------
+# TOPIC 6 - IDK
+
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+
+
+# print(f"The multiplied value is: {multiply(2, 3, 4, 5)}")
